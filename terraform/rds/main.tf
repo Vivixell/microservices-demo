59f590c8ed08e55f3d86e381ed27035a6e1e9077
Vivixell <victorrobin56@gmail.com>
2026-06-24 16:07:40 +0100

    fixed aws terraform version mismatch

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.9"
+      version = "~> 5.90"
     }
     random = {
       source  = "hashicorp/random"
